b223899d5e0f60baa4656a72e7ca3812c3975123
freirart <artur.santos@stone.com.br>
2024-03-11 13:46:36 -0300

    boilerplate n page skeleton

---
+++ tictactoe/src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function TicTacToe() {
+  return (
+    <div className="h-screen flex flex-col">
+      <header className="h-10">Header</header>
+      <main className="flex-1">Main</main>
+      <footer className="h-10">Footer</footer>
+    </div>
+  );
+}
