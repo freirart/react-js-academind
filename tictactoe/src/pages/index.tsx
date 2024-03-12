7e542e5dcb6a3b938d4cc047909ded293da8db62
freirart <artur.santos@stone.com.br>
2024-03-11 22:08:13 -0300

    uses board component

--- tictactoe/src/pages/index.tsx
+++ tictactoe/src/pages/index.tsx
@@ -1,17 +1,19 @@
+import Board from "@/components/Board";
+
 export default function TicTacToe() {
   return (
-    <div className="h-screen flex flex-col bg-light-blue overflow-hidden">
+    <div className="h-screen bg-light-blue overflow-hidden">
       <header className="flex h-1/5 mx-auto w-3/5">
         <div className="mx-auto self-end">Header</div>
       </header>
-      <main className="flex-1 flex w-3/5 h-3/5 mx-auto">
-        <div
-          className="flex justify-center items-center bg-white-blue min-h-[500px]
-            min-w-[500px] m-auto rounded-3xl p-10"
-        >
-          Board
-        </div>
-      </main>
+      <Board
+        playHistory={[
+          { player: "X", x: 1, y: 2 },
+          { player: "O", x: 0, y: 0 },
+          { player: "O", x: 2, y: 2 },
+          { player: "X", x: 0, y: 1 },
+        ]}
+      />
       <footer className="flex h-1/5 mx-auto w-3/5">
         <div className="mx-auto">Footer</div>
       </footer>
