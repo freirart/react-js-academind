a17f43507ff5bb6a1fcd1fecf1d308f8a2a4dfd1
freirart <artur.santos@stone.com.br>
2024-04-30 13:05:36 -0300

    fix color mode switch transition

--- src/components/ColorModeSwitch/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch/ColorModeSwitch.tsx
@@ -12,11 +12,11 @@ export default function ColorModeSwitch({
       onClick={() => onToggle()}
       className="flex absolute bottom-8 right-8 w-12 h-6 rounded-full
         dark:bg-light-blue dark:text-special-black bg-special-black
-        text-white-blue text-center transition-all duration-500 cursor-pointer"
+        text-white-blue text-center cursor-pointer"
     >
       <span
         className={
-          'h-6 w-6 rounded-full shadow-lg ' +
+          'h-6 w-6 rounded-full shadow-lg transition-all duration-200 ' +
           (isDarkMode ? 'bg-white-blue' : 'ml-6 bg-dark-blue')
         }
       >
